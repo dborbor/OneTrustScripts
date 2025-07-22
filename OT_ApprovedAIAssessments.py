54e0034847072de779edc661198d6ce6546dd1e8
dborbor <dborbor@gmail.com>
2025-07-22 16:11:03 -0400

    Create OT_ApprovedAIAssessments.py
    Added main file to process AI assessments from OneTrust

---
+++ OT_ApprovedAIAssessments.py
@@ -0,0 +1,20 @@
+import asyncio
+import logging
+from utils.file_utils import save_all_data
+from utils.onetrust_api import get_filtered_assessment_df
+
+# Configuring logging
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+# Create a logger instance
+logger = logging.getLogger(__name__)
+logging.disable(logging.CRITICAL)  # Comment to view logging // Uncomment to disable all logging
+
+async def main() -> None:
+    inventory_type = "ai_assessments"
+    # Get list of AI assessments from OneTrust API
+    df_offline_sw_assessments = await get_filtered_assessment_df(inventory_type=inventory_type)
+    # Save all offline software assessments to Excel, HTML, and updates the Confluence table
+    save_all_data(df_offline_sw_assessments, inventory_type=inventory_type, status="all")
+
+if __name__ == "__main__":
+    asyncio.run(main())
